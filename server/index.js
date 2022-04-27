ceb2b0c9e664862bbe76d896afa185885de310af
jarenreber <jaren.c.reber@gmail.com>
2022-04-27 16:23:22 -0600

    testing 1000

--- server/index.js
+++ server/index.js
@@ -9,13 +9,7 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4006
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.js'))
-})
-
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/home.css'))
-})
+app.use(express.static('client'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
